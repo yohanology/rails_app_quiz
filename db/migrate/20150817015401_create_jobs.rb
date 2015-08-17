5085d69ee4d1b30193f1bf6cb8a4b08bfbf6bd36
Yohan <yohanology@gmail.com>
2015-08-17 10:17:02 +0800

    Add jobs model, controller and migration

---
+++ db/migrate/20150817015401_create_jobs.rb
@@ -0,0 +1,17 @@
+class CreateJobs < ActiveRecord::Migration
+  def change
+    create_table :jobs do |t|
+      t.string :url
+      t.string :employer_name
+      t.string :employer_description
+      t.string :job_title
+      t.string :job_description
+      t.integer :year_of_experience
+      t.string :education_requirement
+      t.string :industry
+      t.integer :base_salary
+      t.integer :employment_type_id
+      t.timestamps null: false
+    end
+  end
+end
