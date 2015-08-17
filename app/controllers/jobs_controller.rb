1e2802ade201cebcfbdce41983300d4e7ca763db
Yohan <yohanology@gmail.com>
2015-08-17 10:29:36 +0800

    Add jobs create and show routes and controllers

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,4 +3,23 @@ class JobsController < ApplicationController
     jobs = Job.all
     render json: jobs
   end
+
+  def create
+    job = Job.new(job_params)
+    if job.save
+      render json: job, status: 200
+    else
+      render json: { message: "401 Bad Request" }, status: 401
+    end
+  end
+
+  def show
+    job = Job.find(params[:id])
+
+    render json: job
+  end
+
+  def job_params
+      params.require(:job).permit(:title, :content, :category)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'static_pages#index'
   get '/jobs' => 'jobs#index'
+  post '/jobs' => 'jobs#create'
+  get '/jobs/:id' => 'jobs#show'
+
 end
